c1a6b55719def68aaa64e727878f46b090f729f9
Steven Schulteis <schult@gmail.com>
2024-09-16 22:15:24 -0500

    Adjust filter distribution

--- util/PitchShifter.h
+++ util/PitchShifter.h
@@ -10,7 +10,7 @@ class PitchShifter
 public:
     PitchShifter(float sample_rate)
     {
-        for (int i = 0; i < 80; ++i)
+        for (int i = 0; i < 110; ++i)
         {
             const auto center = centerFreq(i);
             const auto bw = bandwidth(i);
@@ -39,7 +39,7 @@ public:
 private:
     static constexpr float centerFreq(const int n)
     {
-        return 480 * gcem::pow(2.0f, (n / 20.0f)) - 420;
+        return 480 * gcem::pow(2.0f, (n / 27.0f)) - 420;
     }
 
     static constexpr float bandwidth(const int n)
